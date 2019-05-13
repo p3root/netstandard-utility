6121e1e72422668d85957df6d991fc05f8775512
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2019-05-13 20:06:05 +0200

    initial commit...

---
+++ src/NetStandardUtils/Platform.cs
@@ -0,0 +1,63 @@
+﻿using System.Runtime.InteropServices;
+
+namespace NetStandardUtils
+{
+    public static class Platform
+    {
+        /// <summary>
+        /// Returns the current running archticture in a .NET like string
+        /// </summary>
+        public static string Arch
+        {
+            get
+            {
+                var arch = "";
+
+                switch (RuntimeInformation.OSArchitecture)
+                {
+                    case Architecture.Arm:
+                        arch += "arm";
+                        break;
+                    case Architecture.Arm64:
+                        arch += "arm64";
+                        break;
+                    case Architecture.X64:
+                        arch += "x64";
+                        break;
+                    case Architecture.X86:
+                        arch += "x86";
+                        break;
+                }
+
+                return arch;
+            }
+        }
+
+        /// <summary>
+        /// returns the runtime identifier
+        /// </summary>
+        public static string Rid
+        {
+            get
+            {
+                var rid = "";
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    rid = "linux-";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    rid = "win-";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    rid = "osx-";
+                }
+
+                rid += Arch;
+
+                return rid;
+            }
+        }
+    }
+}
---
+++ src/NetStandardUtils/Version.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace NetStandardUtils
+{
+    public static class Version
+    {
+        public static string GetAssemblyVersion()
+        {
+            return Assembly.GetCallingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        }
+    }
+}
